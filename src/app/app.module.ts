32ae5912a42a1e7cfac9047ac17b481420256fae
oussamaAlouat <al317257@uji.es>
2020-01-16 12:45:12 +0100

    Added shopping-edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
+    ShoppingEditComponent,
   ],
   imports: [
     BrowserModule,
