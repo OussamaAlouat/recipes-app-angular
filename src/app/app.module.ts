3a4a8d505cc08159cb0a29ea53823f13efb2e436
oussamaAlouat <al317257@uji.es>
2020-01-16 12:41:15 +0100

    Added recipe-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
     HeaderComponent,
     RecipesComponent,
     RecipeListComponent,
+    RecipeDetailComponent,
   ],
   imports: [
     BrowserModule,
