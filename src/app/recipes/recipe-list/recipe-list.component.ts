3dbd7ab4bbe82f96316173226abbc60ca310f077
oussamaAlouat <al317257@uji.es>
2020-01-16 13:30:16 +0100

    Recipe-lis structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-  public recipes: any [];
+  public recipes: Recipe [];
 
   constructor() {
-    this.recipes = [];
+    this.recipes = [
+      new Recipe('A test recipe',
+        'This is a simple test',
+        'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'
+      ),
+    ];
   }
 
   ngOnInit() {
