92766dacd53c43bfba0ea2e7a79f5be74da11223
oussamaAlouat <al317257@uji.es>
2020-01-16 12:40:50 +0100

    Added recipe-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
+    RecipeListComponent,
   ],
   imports: [
     BrowserModule,
