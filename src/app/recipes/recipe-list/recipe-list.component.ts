9d0624c8d14f41dbd7770bcc9a122ef6e7dab5be
oussamaAlouat <al317257@uji.es>
2020-01-17 11:33:54 +0100

    Fixed lint error

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -16,9 +16,9 @@ export class RecipeListComponent implements OnInit {
         'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'
       ),
       new Recipe('A  second test recipe',
-      'This is a simple second test',
-      'https://assets.epicurious.com/photos/5d375cfdae657500091e79e8/6:4/w_620%2Ch_413/TomatoCobbler_RECIPE_071719_7362.jpg'
-    ),
+        'This is a simple second test',
+        'https://assets.epicurious.com/photos/5d375cfdae657500091e79e8/6:4/w_620%2Ch_413/TomatoCobbler_RECIPE_071719_7362.jpg'
+      ),
     ];
   }
 
