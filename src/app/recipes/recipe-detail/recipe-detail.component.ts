12ef74fb32f7abc53aa37388432ab4e23454c074
oussamaAlouat <al317257@uji.es>
2020-01-20 16:18:29 +0100

    Added ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { ShoppingListService } from 'src/app/shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,9 +9,14 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
-  constructor() { }
+  constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
+  onAddToShoppingList() {
+    for(let ingredient of this.recipe.ingredients) {
+      this.shoppingListService.addIngredient(ingredient);
+    }
+  }
 }
