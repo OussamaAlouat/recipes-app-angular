732d934777c6fdded063b02d486b3c8c59857f96
oussamaAlouat <al317257@uji.es>
2020-01-20 15:55:49 +0100

    Added ingredients to recipe detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { Output, EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   private recipes: Recipe [];
@@ -9,11 +10,13 @@ export class RecipeService {
     this.recipes = [
       new Recipe('A test recipe',
         'This is a simple test',
-        'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg'
+        'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg',
+        [new Ingredient('Tomato', 2), new Ingredient('Meat', 1)]
       ),
       new Recipe('A  second test recipe',
         'This is a simple second test',
-        'https://assets.epicurious.com/photos/5d375cfdae657500091e79e8/6:4/w_620%2Ch_413/TomatoCobbler_RECIPE_071719_7362.jpg'
+        'https://assets.epicurious.com/photos/5d375cfdae657500091e79e8/6:4/w_620%2Ch_413/TomatoCobbler_RECIPE_071719_7362.jpg',
+        [new Ingredient('patato', 20)]
       ),
     ];
 
