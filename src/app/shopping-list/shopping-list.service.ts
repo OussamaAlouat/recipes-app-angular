df044a06d8789c9117f1d22b641397c61df22780
oussamaAlouat <al317257@uji.es>
2020-01-20 15:30:31 +0100

    Added content to shoppingList service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,23 @@
-export class ShoppingListService {
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged= new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[];
+
+  constructor() {
+    this.ingredients = [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomatoes', 10)
+    ];
+  }
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
