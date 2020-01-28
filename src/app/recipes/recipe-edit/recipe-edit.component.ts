b37dceac1c43d88cc917eb998f9f5f99dca56e4d
oussamaAlouat <al317257@uji.es>
2020-01-28 09:11:35 +0100

    Delete ingredients of a recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -90,4 +90,8 @@ export class RecipeEditComponent implements OnInit {
   onCancel() {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
+
+  onDeleteIngredient(index: number) {
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
 }
