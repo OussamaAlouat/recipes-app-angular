0c1faa6e194baf7ecb0f1d0289bd5f86b868f998
oussamaAlouat <al317257@uji.es>
2020-01-29 13:35:42 +0100

    Saved the data on firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService){}
+
+  onSaveData() {
+    this.dataStorageService.storeRecipes();
+  }
 }
