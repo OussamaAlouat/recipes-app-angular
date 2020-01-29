8d680aa8fe40bd55154db4c9408756381d2db5d3
oussamaAlouat <al317257@uji.es>
2020-01-29 17:20:35 +0100

    Fixed bug when edit a recipe

--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService  implements Resolve<Recipe[]>{
-  constructor(private dataStorageService: DataStorageService){}
+  constructor(private dataStorageService: DataStorageService,
+    private recipesService: RecipeService){}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipesService.getRecipes();
+    if (recipes.length === 0 ){
+      return this.dataStorageService.fetchRecipes();
+    }
+
+    return recipes;
   }
 }
