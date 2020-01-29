daf250d68c6917f2de46836121da273c393733e3
oussamaAlouat <al317257@uji.es>
2020-01-29 14:55:32 +0100

    Added setter to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -52,4 +52,9 @@ export class RecipeService {
     this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice());
   }
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
