1ae823ab458728580edff4a263a0c509536176ae
oussamaAlouat <al317257@uji.es>
2020-01-30 16:54:22 +0100

    Subscribe to the user subject to know when user is autenticated

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService){}
+export class HeaderComponent implements OnInit, OnDestroy {
+  public isAutenticated: boolean;
+  private userSub: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ){
+    this.isAutenticated = false;
+  }
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(user => {
+      this.isAutenticated = !!user;
+    });
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -15,5 +31,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
