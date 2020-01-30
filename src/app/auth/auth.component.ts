c24ba0ed7f499768c288af85f3d6c9a5868c12c6
oussamaAlouat <al317257@uji.es>
2020-01-30 09:45:18 +0100

    Added a form on auth component

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+  public isLoginMode:boolean;
 
+  constructor() {
+    this.isLoginMode = true;
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(from: NgForm) {
+    console.log('This is the form: ', from);
+  }
 }
