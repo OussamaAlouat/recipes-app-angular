b698e8081c8326f567cc49b1b33dd11d0d4b24a4
oussamaAlouat <al317257@uji.es>
2020-02-03 11:02:41 +0100

    Cleaned code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AuthComponent } from './auth/auth.component';
-
-
 const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
