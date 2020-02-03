d3361016991004c8b686326ec1a0ce76caab0f21
oussamaAlouat <al317257@uji.es>
2020-02-03 10:01:28 +0100

    Removed from routing file, auth route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { AuthComponent } from './auth/auth.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  { path: 'auth', component: AuthComponent }
+  { path: '', redirectTo: '/recipes', pathMatch: 'full'}
 ];
 
 @NgModule({
