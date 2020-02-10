101eb46cd2c8a32cf58851a2568728efc246b1e1
oussamaAlouat <al317257@uji.es>
2020-02-10 14:46:28 +0100

    Added recipesService mock

---
+++ src/__mocks__/RecipeService.component.ts
@@ -0,0 +1,25 @@
+import { Recipe } from 'src/app/recipes/recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Injectable } from '@angular/core';
+import { first } from 'lodash';
+
+@Injectable()
+export class MockRecipeService {
+  recipes: Recipe[];
+
+
+  constructor() {
+    this.recipes = [
+      new Recipe('A test recipe',
+          'This is a simple test',
+          'https://keyassets-p2.timeincuk.net/wp/prod/wp-content/uploads/sites/53/2019/02/Cheesy-mince-pasta-bake.jpg',
+          [new Ingredient('Tomato', 2), new Ingredient('Meat', 1)]
+        ),
+    ];
+  }
+
+  getRecipe(id: number) {
+    return first(this.recipes);
+  }
+
+}
