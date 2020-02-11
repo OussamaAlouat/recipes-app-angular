6febd2b56c316dbdb967ac1b855bb95c8d510112
oussamaAlouat <al317257@uji.es>
2020-02-11 09:35:04 +0100

    Test: Navbar should have some anchors

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -28,5 +28,49 @@ describe('HeaderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('Navbar should have some anchors', () => {
+    it('Should contains an anchor with "Recipe book" as title', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('.navbar-brand').textContent).toContain('Recipe book');
+    });
+
+    it('Should contains an anchor with "Recipes" as title', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('a[href$="/recipes"').textContent).toContain('Recipes');
+    });
+
+    it('Should contains an anchor with "Authenticate" as title', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('a[href$="/auth"').textContent).toContain('Authenticate');
+    });
+
+    it('Should contains an anchor with "Shopping List" as title', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('a[href$="/shopping-list"').textContent).toContain('Shopping List');
+    });
+
+    it('Should contains an anchor with "Manage" as title', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('.dropdown-toggle').textContent).toContain('Manage');
+    });
+
+    it('Should contains an anchor with "Logout" as title', () => {
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('.nav.navbar-nav.navbar-right > li > a').textContent).toContain('Logout');
+    });
+
+    describe('Manage menu should have two anchors', () => {
+      it('One anchor should be "Save data', () => {
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('.dropdown-menu > li').textContent).toContain('Save data');
+      });
+
+      it('The other anchor should be "Fetch data', () => {
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('.dropdown-menu li:last-child').textContent).toContain('Fetch data');
+      });
+    });
+  });
 });
 
