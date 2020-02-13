883076e30683b17eee6ed42caf2f3e3bdf9a541f
oussamaAlouat <al317257@uji.es>
2020-02-13 13:04:47 +0100

    Added button component tests

--- src/app/shared/button/button.component.spec.ts
+++ src/app/shared/button/button.component.spec.ts
@@ -1,25 +1,95 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import { Component, ViewChild, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-test-button',
+  template: `
+    <app-button
+      [label]="label"
+      [type]="type"
+      [disable]="disable"
+      [styleClass]="styleClass"
+    ></app-button>
+  `,
+})
+class HostComponent {
+  @ViewChild(ButtonComponent, { static: true })
+  child: ButtonComponent;
+  label: string;
+  type: string;
+  disable: boolean;
+  styleClass: string;
+
+  constructor() {
+    this.label = 'Save';
+    this.type = 'submit';
+    this.styleClass ='btn btn-success';
+    this.disable = true;
+  }
+}
+
 
 describe('ButtonComponent', () => {
+  let hostcomponent: HostComponent;
   let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+  let fixture: ComponentFixture<HostComponent>;
+  let debugCheckbox;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ButtonComponent ]
+      declarations: [ ButtonComponent, HostComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(HostComponent);
+    hostcomponent = fixture.componentInstance;
+    component = hostcomponent.child;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Button component should work', () => {
+    beforeEach(() => {
+      hostcomponent.disable = false;
+      fixture.detectChanges();
+      debugCheckbox = fixture.debugElement.query(By.css('button')).nativeElement;
+    });
+
+    it('disable option shold been flase', () => {
+      expect(component.disable).toBeFalsy();
+    });
+
+    it('Should emit an event on click', () => {
+      spyOn(component.clicked, 'emit');
+      debugCheckbox.click();
+      fixture.detectChanges();
+      expect(component.clicked.emit).toHaveBeenCalled();
+    });
+  });
+
+  describe('Button component should not work', () => {
+    beforeEach(() => {
+      debugCheckbox = fixture.debugElement.query(By.css('button')).nativeElement;
+    });
+
+    it('disable option shold been true', () => {
+      expect(component.disable).toBeTruthy();
+    });
+
+    it('Should not emit an event on click, when disable is true', () => {
+      spyOn(component.clicked, 'emit');
+      debugCheckbox.click();
+      fixture.detectChanges();
+      expect(component.clicked.emit).not.toHaveBeenCalled();
+    });
   });
 });
