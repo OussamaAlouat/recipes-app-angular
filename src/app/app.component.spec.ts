7f52130208e7e7108dbeee2c9c71e26874ebb4e4
oussamaAlouat <al317257@uji.es>
2020-02-13 15:53:46 +0100

    Remove not necessary code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -39,12 +39,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angularBasics');
   });
-/*
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('angularBasics');
-  });
-  */
 });
