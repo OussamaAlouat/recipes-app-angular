0f0fc456e39ba7c20f38c0c391ee1201ec1842da
oussamaAlouat <al317257@uji.es>
2020-02-13 17:31:10 +0100

    Fixed error on pattern

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -100,7 +100,7 @@ export class RecipeEditComponent implements OnInit {
     (<FormArray>this.recipeForm.get('ingredients')).push(
       new FormGroup({
         'name': new FormControl(null, Validators.required),
-        'amount': new FormControl(null, [Validators.required, Validators.pattern(/s^[1-9]+[0-9]*$/)])
+        'amount': new FormControl(null, [Validators.required, Validators.pattern(/^[1-9]+[0-9]*$/)])
       })
     )
   }
