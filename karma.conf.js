32554b4047f36bce32a7685b2fbfb1c13b6dd59b
oussamaAlouat <al317257@uji.es>
2020-02-17 11:28:37 +0100

    Added some config to avoid some errors

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,8 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
+      random: true
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/angularBasics'),
