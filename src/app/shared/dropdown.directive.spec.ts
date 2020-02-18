eab65fa343347a918898443b659a74a393230a94
oussamaAlouat <al317257@uji.es>
2020-02-18 10:20:31 +0100

    Added dropdown directive test

---
+++ src/app/shared/dropdown.directive.spec.ts
@@ -0,0 +1,39 @@
+import { Component, DebugElement } from "@angular/core";
+import { DropdownDirective } from './dropdown.directive';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-test',
+  template: `<li class="dropdown" appDropdown></li>`,
+})
+
+class HostComponent {}
+
+describe('DropDown directive', () => {
+  let component: HostComponent;
+  let fixture: ComponentFixture<HostComponent>;
+  let liElement: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HostComponent, DropdownDirective]
+    });
+
+    fixture = TestBed.createComponent(HostComponent);
+    component = fixture.componentInstance;
+    liElement = fixture.debugElement.query(By.css('li'));
+  });
+
+  it('Should not appear class open', () => {
+    const element = liElement.nativeElement;
+    expect(element.className).not.toContain('open');
+  });
+
+  it('Should appear class open', () => {
+    const element = liElement.nativeElement;
+    element.click();
+    fixture.detectChanges();
+    expect(element.className).toContain('open');
+  });
+});
