e87dcfa8f7bdc595f64e87e4b54ad8fc18d3dd8a
oussamaAlouat <al317257@uji.es>
2020-02-21 11:47:25 +0100

    Load recipes on init recipes component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -9,7 +10,9 @@ import { Recipe } from './recipe.model';
 
 export class RecipesComponent implements OnInit {
   public recivedRecipe: Recipe;
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.recipeService.fetchFromServer();
+  }
 }
