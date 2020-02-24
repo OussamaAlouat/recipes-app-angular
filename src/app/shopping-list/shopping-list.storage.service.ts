5089c32e6c16cd081774f70184a197aabe290704
oussamaAlouat <al317257@uji.es>
2020-02-24 09:38:47 +0100

    Added ingredients storagse service

---
+++ src/app/shopping-list/shopping-list.storage.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Ingredient } from '../shared/ingredient.model';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ShoppingListStorageService{
+  constructor(private http: HttpClient) {}
+
+  saveIngredient(ingredient: Ingredient) {
+    return this.http.post('http://localhost:3000/ingredients', ingredient);
+  }
+}
